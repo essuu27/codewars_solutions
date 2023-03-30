dc904d4ed5ee31efe32698ea31ce031990f12b29
essuu27 <86964215+essuu27@users.noreply.github.com>
2023-03-30 23:13:42 +0100

    Create string_ends_with.py
    Adding a kata

---
+++ 7kyu/string_ends_with.py
@@ -0,0 +1,5 @@
+# Code requirement:
+# Return true if the first argument(string) passed in ends with the 2nd argument (also a string)
+
+def solution(text, ending):
+  return bool(text.endswith(ending))
