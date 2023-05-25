a4b9255a968ceecc04200dd8c36d2719105a1eb8
essuu27 <86964215+essuu27@users.noreply.github.com>
2023-05-25 12:12:36 +0100

    Delete vowel_count.py

--- 7kyu/vowel_count.py
+++
@@ -1,9 +0,0 @@
-# Return the number/count of vowels in the given string.
-
-def get_count(sentence):
-    vowels='aeiou'
-    cnt = 0
-    for i in vowels:
-        cnt += sentence.count(i)
-    return(cnt)
-    
