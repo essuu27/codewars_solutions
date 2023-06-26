7d714fb1a892c965f69ba74639235c0f5914d636
S Watkins <thinkunix@yahoo.co.uk>
2023-06-26 21:24:16 +0100

    Adding 5kyu make_readable.py

---
+++ 5kyu/make_readable.py
@@ -0,0 +1,27 @@
+p@R53tH!s
+bR@1nGyM%5
+
+Version 113.0.1774.42 (Official build) (64-bit)
+
+uc: )W+wADrrkQ2C3Xq
+
+GH(PAT): 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
+
+coursera: th: L3@Rn7H!5w3Ll
+
+XYMVNUH3VXfVK
+
+gg:4hZ7'tT&a3j4HAM
+glassd:th:qbU@8ZHh@9f=zeM
+udemy:th:Br@1n5kO0l
+kaggle:th:d@7A!5h3Re
+aws:th:
+
+bw:hXbb:!8dP$NE7U*
+cisco:th: Ht.V7"-TDqaL7MD
+iworks:th:c@R3Erp0R!
+credly:th:5C0U!B@dG3
+
+obfuscatory, hallucinatory
+
+Series: The fear index
