38bfb5048f7270b7daeb62147bf888632d91bfa5
S Watkins <thinkunix@yahoo.co.uk>
2023-07-27 23:13:58 +0100

    Add 8kyu/lovefunc.py

---
+++ 8kyu/lovefunc.py
@@ -0,0 +1,5 @@
+# Check the number of petals on two flowers. Return False if they both both odd or,
+# both even. Return True if one is odd and the other even.
+
+def lovefunc( flower1, flower2 ):
+    print(not (flower1 % 2) == (flower2 % 2))
