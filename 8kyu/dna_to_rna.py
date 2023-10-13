457545de09ae381c8c73404be35aa5f19b648f58
S Watkins <thinkunix@yahoo.co.uk>
2023-10-13 18:54:33 +0100

    Adding 8kyu/dna_to_rna.py

---
+++ 8kyu/dna_to_rna.py
@@ -0,0 +1,6 @@
+# DNA is made of four base chemical (A,C,G,T). Write a function that takes a DNA molecule
+# and "converts" it to RNA by swapping every "T" base with a different nucleic acid base "U"
+
+def dna_to_rna(dna):
+    return (dna.replace("T","U"))
+
