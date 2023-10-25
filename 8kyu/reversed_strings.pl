dbac7ba25314ee07e8361ebef9fdbe912979edb6
S Watkins <thinkunix@yahoo.co.uk>
2023-10-25 19:03:14 +0100

    Add 8kyu/reversed_strings.pl

---
+++ 8kyu/reversed_strings.pl
@@ -0,0 +1,13 @@
+# Complete the solution so that it reverses the string passed into it.
+
+package Kata;
+use strict;
+use warnings;
+
+sub solution {
+    my ($str) = @_;
+    my $rev = reverse $str;
+    return $rev
+}
+
+1; 
