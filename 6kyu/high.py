a0d2f4dbc2334b5c4b28fea60828172540cb765a
essuu27 <86964215+essuu27@users.noreply.github.com>
2024-05-31 19:46:54 +0100

    Delete 6kyu/high.py

--- 6kyu/high.py
+++
@@ -1,6 +0,0 @@
-# Given a string of words, you need to find the highest scoring word.Each letter of
-# a word scores points according to its position in the alphabet
-
-def high(x):
-    words = x.split(' ')
-    return max(words, key=lambda word: sum(ord(char) - 96 for char in word))
