ba2545cefef57c8d2bf687c7f6e1f765e45ceaa4
essuu27 <86964215+essuu27@users.noreply.github.com>
2024-05-31 19:50:40 +0100

    Delete 6kyu/tribonacci.py

--- 6kyu/tribonacci.py
+++
@@ -1,9 +0,0 @@
-# Write a function that works basically like a Fibonacci, but summing the last 3
-# (instead of 2) numbers of the sequence to generate the next.
-
-def tribonacci(signature, n):
-    if n < 1:
-        return []
-    for _ in range(n - 3):
-        signature.append(sum(signature[-3:]))
-    return signature[:n]
