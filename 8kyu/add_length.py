4a44948d686cb4e1d8478dcf7c366cf8058da0ed
Steff Watkins <essuu27@github.com>
2024-11-26 16:05:42 +0000

    Triggering workflow action

--- 8kyu/add_length.py
+++ 8kyu/add_length.py
@@ -1,8 +1,7 @@
 # Write a function that takes in a line, splits it into individual words and returns
-# an array made up of those words following by a space and their length
+# an array made up of those words following by a space and their length.
 
 def add_length(str_):
     x = str_.split(" ")
     str_ = [ (f"{y} {len(y)}") for y in x ]
     return(str_)
-
